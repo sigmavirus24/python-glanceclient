7d7e6edaf8340d3d5e0920a510cd2a7772cfaf0d
Stuart McLaren <stuart.mclaren@hp.com>
2015-06-08 14:49:52 +0000

    Remove custom SSL compression handling
    Custom SSL handling was introduced because disabling SSL layer compression
    provided an approximately five fold performance increase in some
    cases. Without SSL layer compression disabled the image transfer would be
    CPU bound -- with the CPU performing the DEFLATE algorithm.  This would
    typically limit image transfers to < 20 MB/s. When --no-ssl-compression
    was specified the client would not negotiate any compression algorithm
    during the SSL handshake with the server which would remove the CPU
    bottleneck and transfers could approach wire speed.
    
    In order to support '--no-ssl-compression' two totally separate code
    paths exist depending on whether this is True or False.  When SSL
    compression is disabled, rather than using the standard 'requests'
    library, we enter some custom code based on pyopenssl and httplib in
    order to disable compression.
    
    This patch/spec proposes removing the custom code because:
    
    * It is a burden to maintain
    
     Eg adding new code such as keystone session support is more complicated
    
    * It can introduce additional failure modes
    
     We have seen some bugs related to the 'custom' certificate checking
    
    * Newer Operating Systems disable SSL for us.
    
     Eg. While Debian 7 defaulted to compression 'on', Debian 8 has compression
     'off'. This makes both servers and client less likely to have compression
     enabled.
    
    * Newer combinations of 'requests' and 'python' do this for us
    
     Requests disables compression when backed by a version of python which
     supports it (>= 2.7.9). This makes clients more likely to disable
     compression out-of-the-box.
    
    * It is (in principle) possible to do this on older versions too
    
     If pyopenssl, ndg-httpsclient and pyasn1 are installed on older
     operating system/python combinations, the requests library should
     disable SSL compression on the client side.
    
    * Systems that have SSL compression enabled may be vulnerable to the CRIME
    (https://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2012-4929) attack.
    Installations which are security conscious should be running the Glance
    server with SSL disabled.
    
    Full Spec: https://review.openstack.org/#/c/187674
    Blueprint: remove-custom-client-ssl-handling
    
    Change-Id: I7e7761fc91b0d6da03939374eeedd809534f6edf

--- glanceclient/tests/functional/test_readonly_glance.py
+++ glanceclient/tests/functional/test_readonly_glance.py
@@ -70,3 +70,11 @@ class SimpleReadOnlyGlanceClientTest(base.ClientTestBase):
 
     def test_debug_list(self):
         self.glance('image-list', flags='--debug')
+
+    def test_no_ssl_compression(self):
+        # Test deprecating this hasn't broken anything
+        out = self.glance('--no-ssl-compression image-list')
+        endpoints = self.parser.listing(out)
+        self.assertTableStruct(endpoints, [
+            'ID', 'Name', 'Disk Format', 'Container Format',
+            'Size', 'Status'])
